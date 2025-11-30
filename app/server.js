8d90e4deffbc25b1227ea057359800847436cf03
Arseny Avseenko <avsarsen@gmail.com>
2025-11-30 17:55:43 -0500

    added x-ray

--- app/server.js
+++ app/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const AWSXRay = require("aws-xray-sdk");
+
+// capture all outgoing AWS SDK calls (if any later)
+AWSXRay.captureHTTPsGlobal(require("http"));
+// capture express middleware
 const app = express();
+app.use(AWSXRay.express.openSegment("ecs-hello-world-app"));
 
-app.get("/", (req, res) => res.send("Hello from ECS!"));
+app.get("/", (req, res) => res.send("Hello from ECS with X-Ray!"));
 
-app.listen(80, () => console.log("Running on port 80"));
+app.use(AWSXRay.express.closeSegment());
+
+app.listen(80, () => console.log("Running on port 80 with X-Ray"));
